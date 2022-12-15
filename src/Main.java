ff7c058a3279ff44a091b359e0fec9b94b2e6dfa
Nikita Bolshakov <bnikmik@gmail.com>
2022-12-15 12:52:35 +0300

    Курсовая работа 1: Базовая версия.

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,73 @@
 public class Main {
+    static Employee[] employees = new Employee[10];
+
+    public static void printEmployeesList() {
+        System.out.println("Список всех сотрудников со всеми данными:");
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee);
+            }
+        }
+    }
+
+    public static double printTotalSalaries() {
+        double totalSalaries = 0;
+        for (Employee employee : employees) {
+            if (employee != null) {
+                totalSalaries += employee.getSalary();
+            }
+        }
+        return totalSalaries;
+    }
+
+    public static double printAverageSalary() {
+        return printTotalSalaries() / Employee.getCount();
+    }
+
+    public static Employee findEmployeeWithMinSalary() {
+        double minSalary = Double.MAX_VALUE;
+        int temp = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].getSalary() < minSalary) {
+                minSalary = employees[i].getSalary();
+                temp = i;
+            }
+        }
+        return employees[temp];
+    }
+
+    public static Employee findEmployeeWithMaxSalary() {
+        double maxSalary = Double.MIN_VALUE;
+        int temp = 0;
+        for (int i = 0; i < employees.length; i++) {
+            if (employees[i] != null && employees[i].getSalary() > maxSalary) {
+                maxSalary = employees[i].getSalary();
+                temp = i;
+            }
+        }
+        return employees[temp];
+    }
+
+    public static void printEmployeesFullName() {
+        System.out.println("Список ФИО всех сотрудников:");
+        for (Employee employee : employees) {
+            if (employee != null) {
+                System.out.println(employee.getFullName());
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        employees[0] = new Employee("Голубев Касьян Михайлович", 1, 10_023);
+        employees[1] = new Employee("Гурьев Федор Леонидович", 1, 15_023);
+        employees[2] = new Employee("Аксёнов Аввакум Егорович", 1, 3_0132);
+        employees[3] = new Employee("Стрелков Юлий Андреевич", 3, 21_125);
+        employees[4] = new Employee("Гришин Савелий Валерьевич", 2, 21_500);
+        printEmployeesList();
+        System.out.println("Сумма всех заработных плат: " + printTotalSalaries());
+        System.out.println("Среднее значение зарплат: " + printAverageSalary());
+        System.out.println("Минимальная зарплата - " + findEmployeeWithMinSalary());
+        System.out.println("Максимальная зарплата - " + findEmployeeWithMaxSalary());
+        printEmployeesFullName();
     }
 }
